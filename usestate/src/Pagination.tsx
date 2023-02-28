cfaf7fe8a1d7e150d8b76e48dcb854762c8ca11b
= <shawaarma0200@gmail.com>
2023-02-28 23:17:55 +0300

    com

--- usestate/src/Pagination.tsx
+++ usestate/src/Pagination.tsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const sliding = ["slide1", "slide2", "slide3"];
 
 export default function Pagination() {
-    return (
-        <div className=' w-screen h-screen bg-gradient-to-tr from-blue-300 to-neutral-50  p-5'>
-            <div className=' w-full h-full  bg-slate-900 backdrop-blur-lg flex gap-3 justify-center    p-10 flex-wrap  items-center'>
-              <div className='   w-11/12  h-5/6 bg-white'>
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-                  <img />
-              </div>
+  const handlePrevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? sliding.length - 1 : currentSlide - 1);
+  };
+
+  const handleNextSlide = () => {
+    setCurrentSlide(currentSlide === sliding.length - 1 ? 0 : currentSlide + 1);
+  };
+
+  return (
+    <div className="w-screen h-screen bg-gradient-to-tr from-blue-300 to-neutral-50 gap-3 p-5">
+      <div className="w-full h-1/2 flex justify-center gap-4 overflow-hidden">
+        {sliding.map((s, index) => {
+          const slideStyle = {
+            transform: `translateX(${
+              (index - currentSlide) * 100 // move slide left or right based on the current slide index
+            }%)`,
+          };
+          return (
+            <div key={index} className="bg-slate-500 h-full w-11/12" style={slideStyle}>
+              {s}
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+      <div className="flex justify-between p-5">
+        <button onClick={handlePrevSlide}>Prev</button>
+        <button onClick={handleNextSlide}>Next</button>
+      </div>
+    </div>
+  );
 }
